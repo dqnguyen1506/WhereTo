b3865cbc060d43e1fe9263c139f6ee41dd2afda7
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2022-02-15 09:56:16 -0800

    Update

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
-} from "react-router-dom";
+  Switch
+} from 'react-router-dom';
 
-import NewPlace from "./places/pages/NewPlace";
-import Users from "./user/pages/Users";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
