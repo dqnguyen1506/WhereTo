a2d44265789014a414531377476cf440837c9205
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2022-02-22 18:49:28 -0800

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -8,7 +11,11 @@ const HttpError = require("./models/http-error");
 
 const app = express();
 
+//http request body parser into js objects => next(parsedBody)
 app.use(bodyParser.json());
+
+//express's middleware to serve/return the files in uploads & images folders
+app.use("/uploads/images", express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -21,28 +28,42 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+//register routes middlewares
+app.use("/api/places", placesRoutes); // only forward request with path => /api/places/... to placesRoutes
+app.use("/api/users", usersRoutes); //only forward request with path => /api/users/... to userRoutes
 
+//default error handling middlware for requests w/o a response
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError("Could not find this route", 404);
   throw error;
 });
 
+// error handling middleware fuction when an error was thrown ==> must include "error"
 app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
+  //if there is a file ==> when there's sign up error ==> delete the newly uploaded file
+  if (req.file) {
+    //delete the file
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
   }
+
+  //if response sent, cannot do anything because cannot send more than responses
+  if (res.headerSent) {
+    return next(error); //forward the error to next middleware
+  }
+  //no response sent
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.json({ message: error.message || "An unknown error occured!" });
 });
 
+//if database connection is successful, start express server
 mongoose
   .connect(
-    "mongodb+srv://dungqnguyen1506:dung123456@cluster0.auyhf.mongodb.net/WhereTo?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    "mongodb+srv://dungqnguyen1506:dung123456@cluster0.auyhf.mongodb.net/WhereTo?retryWrites=true&w=majority"
   )
   .then(() => {
+    //server on port 5000
     app.listen(5000);
   })
   .catch((err) => {
